1e2eefda50b547ab7935bce1d0dcdc8ad88fa3d6
vitorsoaresdrx-a11y <vitorsoaresdrx@gmail.com>
2026-03-16 09:43:45 -0300

    Add PontoCadastro component with FaceEnrollment

---
+++ src/pages/PontoCadastro.tsx
@@ -0,0 +1,9 @@
+import FaceEnrollment from "@/components/FaceEnrollment";
+
+export default function PontoCadastro() {
+  return (
+    <div className="p-6">
+      <FaceEnrollment />
+    </div>
+  );
+}
